a55a5cbf83240baf55f4bd20ce77d1bb9a1c7e76
Xianzhi Liu <smalltigerson@gmail.com>
2016-10-18 15:59:23 -0500

    complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
 
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :sort, :ratings)
   end
 
   def show
@@ -10,8 +10,47 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
+  def defrating
+    @all_ratings=['G','PG','PG-13','R']
+    @all_ratings
+  end
+
   def index
-    @movies = Movie.all
+    @all_ratings=defrating
+    @keys=[nil]
+    if(params[:ratings]!=nil)
+      rating=params[:ratings]
+      session[:ratings]=rating
+      session[:sort]=nil
+    elsif(session[:ratings]!=nil && params[:sort]==nil)   #have to be && to avoid mess
+      rating=session[:ratings]
+      flash.keep
+      redirect_to movies_path(:ratings=>rating)
+    else
+      rating=nil
+    end
+    if(params[:sort]!=nil)
+      sort=params[:sort]
+      session[:sort]=sort
+      session[:ratings]=nil
+    elsif(session[:sort]!=nil && params[:ratings]==nil)
+      sort=session[:sort]
+      flash.keep
+      redirect_to movies_path(:sort=>sort)
+    else
+      sort=nil
+    end
+    
+    if(sort=='title' || sort=='release_date')
+      @movies = Movie.order(sort)
+      @sort=sort;
+    elsif(rating.nil?)
+      @movies = Movie.all
+    else
+      keys=rating.keys
+      @keys=keys
+      @movies = Movie.where(rating: keys)
+    end
   end
 
   def new
